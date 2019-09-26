719331b5fd14905e9f459f75105e031c07da3905
shahriar0247 <47810596+shahriar0247@users.noreply.github.com>
2019-09-26 22:48:23 +0600

    Add files via upload

---
+++ Simple hacking helper/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_hacking_helper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+     
+        public int textvalue = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = textvalue.ToString();
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            textvalue = textvalue + 1;
+            textBox1.Text = textvalue.ToString();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
